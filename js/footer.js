f2dadfe83c3c99754700366343412dfd0785f2f6
Camellia379 <2021141530150@stu.scu.edu.cn>
2024-06-09 22:20:36 +0800

    Site updated: 2024-06-09 22:20:36

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+// 动态心跳
+$(document).ready(function(e){
+    $('.copyright').html('©2023 - 2024 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By 小饼干');
+})
+
+$(document).ready(function(e){
+    show_date_time();
+})
+
+
